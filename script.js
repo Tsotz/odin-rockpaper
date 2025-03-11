3fe65b21806d8b1ec71db04c8e0f1a7afd723905
Tsotz <201083717+Tsotz@users.noreply.github.com>
2025-03-11 21:05:03 +0300

    game done just needs rounds

--- script.js
+++ script.js
@@ -1,50 +1,73 @@
-function getComputerChoice () {
+function getComputerChoice() {
+    let computerChoice = Math.random();
+    
 
-    let rps = Math.random();
-
-    if (rps >= 0 && rps <= 0.33) {
-        
+    if (computerChoice >= 0 && computerChoice <= 0.33) {
+        console.log("Computer chose: rock");  
         return "rock";
-
-    } else if (rps > 0.33 && rps <= 0.66) {
-
+    } else if (computerChoice > 0.33 && computerChoice <= 0.66) {
+        console.log("Computer chose: paper");  
         return "paper";
-
-    } else if (rps > 0.66 && rps <= 1) {
-        
+    } else if (computerChoice > 0.66 && computerChoice <= 1) {
+        console.log("Computer chose: scissors");  
         return "scissors";
-
     }
-
- 
-
 }
-console.log(getComputerChoice());
+
 
 
 
 function getHumanChoice () {
 
-    return parseInt(prompt("rock, paper or scissors?"));
+    let humanChoice = prompt("rock, paper or scissors?");
+    humanChoice = humanChoice.toLowerCase().trim();
+    console.log(`Player choice: '${humanChoice}'`);  
+    return humanChoice;
+    
 
 }
-
-
-console.log(getHumanChoice());
 
 
 let humanScore = 0;
 let computerScore = 0;
 
 
-function playRound (HumanChoice.toLowerCase(), computerChoice) {
 
-    
+
+function playRound (humanChoice, computerChoice) {
+
+  
+
+    if (humanChoice === computerChoice) {
+
+        return " It's a tie! ";
+
+    } else if ((humanChoice === "scissors" && computerChoice === "paper") || 
+               (humanChoice === "paper" && computerChoice === "rock") || 
+               (humanChoice === "rock" && computerChoice === "scissors") ) 
+                
+                {
+
+        humanScore++;
+        return "You Win!";
+        
+    } else {
+        
+        computerScore++;
+        return "You Lose!";
+        
+    }
 
 }
 
+
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
+
+
+console.log(playRound(humanSelection, computerSelection));
+console.log(humanScore);
+
 
 
 
