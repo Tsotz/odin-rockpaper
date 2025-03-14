211cfa1fcec9e35f037ed230eb8705192467153e
Tsotz <201083717+Tsotz@users.noreply.github.com>
2025-03-14 15:35:11 +0300

    all done

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const buttons = document.querySelectorAll("button");
 
+const cont  = document.querySelector(".container");
+const score = document.createElement("div");
+score.classList.add("score");
+score.setAttribute("style", "background-color:blue; color:black; border:2px solid purple;");
+cont.appendChild(score);
+
+const choice = document.createElement("div");
+choice.classList.add("choice");
+choice.setAttribute("style", "background-color:blue; color:black; border:2px solid purple;");
+cont.appendChild(choice);
+
+const game = document.createElement("div");
+game.classList.add("game");
+game.setAttribute("style", "background-color:blue; color:black; border:2px solid purple;");
+cont.appendChild(game);
+
 let humanScore = 0;
 let computerScore = 0;
 
@@ -9,14 +25,11 @@ function getComputerChoice() {
     let computerChoice = Math.random();
     
 
-    if (computerChoice >= 0 && computerChoice <= 0.33) {
-        console.log("Computer chose: rock");  
+    if (computerChoice >= 0 && computerChoice <= 0.33) {      
         return "rock";
-    } else if (computerChoice > 0.33 && computerChoice <= 0.66) {
-        console.log("Computer chose: paper");  
+    } else if (computerChoice > 0.33 && computerChoice <= 0.66) {   
         return "paper";
     } else if (computerChoice > 0.66 && computerChoice <= 1) {
-        console.log("Computer chose: scissors");  
         return "scissors";
     }
 }
@@ -53,11 +66,31 @@ buttons.forEach(button => {
 
     button.addEventListener("click", () => {
 
+        if (humanScore >= 5 || computerScore >= 5) return;
+
         const humanChoice = button.textContent.toLowerCase().trim();
         const computerChoice = getComputerChoice();
-        console.log(playRound(humanChoice, computerChoice));
-        console.log(`Player Score : ${humanScore}  Computer Score : ${computerScore}`);
+        choice.textContent = `Player Chose : ${humanChoice} | Computer Chose : ${computerChoice}`;
+        game.textContent = playRound(humanChoice, computerChoice);
+        if (humanScore === 5) {
 
-    })
+            score.textContent = "Player Won The Game!";
 
-})
+        } else if (computerScore === 5) {
+
+            score.textContent = "Computer Won the Game";
+
+        } else {
+            
+           score.textContent = (`Player Score : ${humanScore}  Computer Score : ${computerScore}`);
+
+        }
+    });
+
+});
+
+
+
+
+
+
